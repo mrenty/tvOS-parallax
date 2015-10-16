7cc6c8ba61f4bbc5e97396dc4f0b82bd55ab0756
Michiel Renty <m.renty@gmail.com>
2015-10-16 17:44:44 +0200

    Adjusted radius

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 			var tiltx = - (dy / cy);
 			var tilty = (dx / card.offsetWidth / 2.0);
 			var radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-			var degree = (radius * 100);
+			var degree = (radius * 40);
 
 			card.style.setProperty('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
 			card.style.setProperty('transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
