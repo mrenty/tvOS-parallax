472eb8c5d16e0889c0a20aa7be3f5a88f8761911
Michiel Renty <m.renty@gmail.com>
2015-10-16 17:42:58 +0200

    Refactored script

--- script.js
+++ script.js
@@ -3,18 +3,15 @@
 		Card.prototype.hover = function(e) {
 			var card = e.currentTarget;
 
-			var bodyRect = document.body.getBoundingClientRect();
-			var cardRect = card.getBoundingClientRect();
-
-			var cx = Math.ceil(cardRect.left - bodyRect.left + (cardRect.width / 2.0));
-			var cy = Math.ceil(cardRect.top - (bodyRect.top + window.scrollY) + (cardRect.height / 2.0));
-			var dx = e.clientX - cx;
-			var dy = e.clientY - cy;
+			var cx = Math.ceil(card.offsetLeft + (card.offsetWidth / 2.0));
+			var cy = Math.ceil(card.offsetTop - window.pageYOffset + (card.offsetHeight / 2.0));
+			var dx = e.pageX - cx;
+			var dy = e.pageY - window.pageYOffset - cy;
 
 			var tiltx = - (dy / cy);
-			var tilty = (dx / cx);
+			var tilty = (dx / card.offsetWidth / 2.0);
 			var radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-			var degree = (radius * 40);
+			var degree = (radius * 100);
 
 			card.style.setProperty('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
 			card.style.setProperty('transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
@@ -23,9 +20,9 @@
 
 		Card.prototype.reset = function(e) {
 			var card = e.currentTarget;
-			 card.style.setProperty('-webkit-transform', 'rotate3d(0, 0, 0, 0deg)');
-			 card.style.setProperty('transform', 'rotate3d(0, 0, 0, 0deg)');
-			 card.style.transition = '-webkit-transform .5s, transform .5s';
+			card.style.setProperty('-webkit-transform', 'rotate3d(0, 0, 0, 0deg)');
+			card.style.setProperty('transform', 'rotate3d(0, 0, 0, 0deg)');
+			card.style.transition = '-webkit-transform .5s, transform .5s';
 		};
 	}
 }());
