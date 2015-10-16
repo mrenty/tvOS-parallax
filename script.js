2893e9b6299d9279324b92dfa9793b0ea7716973
Michiel Renty <m.renty@gmail.com>
2015-10-16 18:01:51 +0200

    Added support for custom slack

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
 	this.Card = function() {
-		Card.prototype.hover = function(e) {
+		Card.prototype.hover = function(e, slack) {
 			var card = e.currentTarget;
 
 			var cx = Math.ceil(card.offsetLeft + (card.offsetWidth / 2.0));
@@ -11,7 +11,7 @@
 			var tiltx = - (dy / cy);
 			var tilty = (dx / card.offsetWidth / 2.0);
 			var radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
-			var degree = (radius * 40);
+			var degree = (radius * parseInt(slack));
 
 			card.style.setProperty('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
 			card.style.setProperty('transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
